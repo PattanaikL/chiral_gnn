2022e3c5b966e3e40b07efec48cd1de587295bee
LPattanaik <lagnajit@mit.edu>
2020-07-14 16:40:31 -0400

    added tetra modules

---
+++ model/tetra.py
@@ -0,0 +1,99 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import copy
+
+
+class TetraPermuter(nn.Module):
+
+    def __init__(self, hidden, device):
+        super(TetraPermuter, self).__init__()
+
+        self.W_bs = nn.ModuleList([copy.deepcopy(nn.Linear(hidden, hidden)) for _ in range(4)])
+        self.device = device
+        self.drop = nn.Dropout(p=0.2)
+
+        self.tetra_perms = torch.tensor([[0, 1, 2, 3],
+                                         [0, 2, 3, 1],
+                                         [0, 3, 1, 2],
+                                         [1, 0, 3, 2],
+                                         [1, 2, 0, 3],
+                                         [1, 3, 2, 0],
+                                         [2, 0, 1, 3],
+                                         [2, 1, 3, 0],
+                                         [2, 3, 0, 1],
+                                         [3, 0, 2, 1],
+                                         [3, 1, 0, 2],
+                                         [3, 2, 1, 0]])
+
+    def forward(self, x):
+        nei_messages = torch.zeros([x.size(0), x.size(2)]).to(self.device)
+
+        for p in self.tetra_perms:
+            nei_messages_list = [self.drop(F.relu(l(t))) for l, t in zip(self.W_bs, torch.split(x[:, p, :], 1, dim=1))]
+            nei_messages += self.drop(F.relu(torch.cat(nei_messages_list, dim=1).sum(dim=1)))
+        return nei_messages / 12.
+
+
+class ConcatTetraPermuter(nn.Module):
+
+    def __init__(self, hidden, device):
+        super(ConcatTetraPermuter, self).__init__()
+
+        self.W_bs = nn.Linear(hidden*4, hidden)
+        torch.nn.init.xavier_normal_(self.W_bs.weight, gain=1.0)
+        self.hidden = hidden
+        self.device = device
+        self.drop = nn.Dropout(p=0.2)
+
+        self.tetra_perms = torch.tensor([[0, 1, 2, 3],
+                                         [0, 2, 3, 1],
+                                         [0, 3, 1, 2],
+                                         [1, 0, 3, 2],
+                                         [1, 2, 0, 3],
+                                         [1, 3, 2, 0],
+                                         [2, 0, 1, 3],
+                                         [2, 1, 3, 0],
+                                         [2, 3, 0, 1],
+                                         [3, 0, 2, 1],
+                                         [3, 1, 0, 2],
+                                         [3, 2, 1, 0]])
+
+    def forward(self, x):
+
+        nei_messages = torch.zeros([x.size(0), x.size(2)]).to(self.device)
+
+        for p in self.tetra_perms:
+            nei_messages += self.drop(F.relu(self.W_bs(x[:, p, :].view(x.size(0), self.hidden*4))))
+        return nei_messages / 12.
+
+
+class TetraDifferencesProduct(nn.Module):
+
+    def __init__(self):
+        super(TetraDifferencesProduct, self).__init__()
+
+    def forward(self, x):
+
+        indices = torch.arange(4).to(x.device)
+        message_tetra_nbs = [x.index_select(dim=1, index=i).squeeze() for i in indices]
+        message_tetra = torch.ones_like(message_tetra_nbs[0])
+
+        # note: this will zero out reps for chiral centers with multiple carbon neighbors on first pass
+        for i in range(4):
+            for j in range(i + 1, 4):
+                message_tetra = torch.mul(message_tetra, (message_tetra_nbs[i] - message_tetra_nbs[j]))
+        message_tetra = torch.sign(message_tetra) * torch.pow(torch.abs(message_tetra) + 1e-6, 1 / 6)
+        return message_tetra
+
+
+def get_tetra_update(args):
+
+    if args.message == 'tetra_permute':
+        return TetraPermuter(args.hidden_size, args.device)
+    elif args.message == 'tetra_permute_concat':
+        return ConcatTetraPermuter(args.hidden_size, args.device)
+    elif args.message == 'tetra_pd':
+        return TetraDifferencesProduct()
+    else:
+        raise ValueError("Invalid message type.")
