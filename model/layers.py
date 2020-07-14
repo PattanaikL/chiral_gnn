001b0b1bd1b8c1eb0158fc165cb3d6ed819c68ba
LPattanaik <lagnajit@mit.edu>
2020-07-14 16:41:48 -0400

    GCN tetra operations

--- model/layers.py
+++ model/layers.py
@@ -1,9 +1,12 @@
 from torch_geometric.nn import MessagePassing
 from torch_geometric.utils import degree
+from torch_geometric.utils import to_dense_adj
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
+from .tetra import get_tetra_update
 
 
 class GCNConv(MessagePassing):
@@ -11,8 +14,12 @@ class GCNConv(MessagePassing):
         super(GCNConv, self).__init__(aggr='add')
         self.linear = nn.Linear(args.hidden_size, args.hidden_size)
         self.batch_norm = nn.BatchNorm1d(args.hidden_size)
+        self.tetra = args.tetra
+        if self.tetra:
+            self.tetra_update = get_tetra_update(args)
 
-    def forward(self, x, edge_index, edge_attr):
+    def forward(self, x, edge_index, edge_attr, parity_atoms):
+
         # no edge updates
         x = self.linear(x)
 
@@ -22,10 +29,38 @@ class GCNConv(MessagePassing):
         deg_inv_sqrt = deg.pow(-0.5)
         norm = deg_inv_sqrt[row] * deg_inv_sqrt[col]
         x = self.propagate(edge_index, x=x, edge_attr=edge_attr, norm=norm)
+
+        if self.tetra:
+            tetra_ids = parity_atoms.nonzero().squeeze()
+            x[tetra_ids] = self.tetra_message(x, edge_index, edge_attr, tetra_ids, parity_atoms)
+
         return self.batch_norm(x), edge_attr
 
     def message(self, x_j, edge_attr, norm):
         return norm.view(-1, 1) * F.relu(x_j + edge_attr)
+
+    def tetra_message(self, x, edge_index, edge_attr, tetra_ids, parity_atoms):
+
+        row, col = edge_index
+        tetra_nei_ids = torch.cat([row[col == i].unsqueeze(0) for i in range(x.size(0)) if i in tetra_ids])
+
+        # calculate pseudo tetra degree aligned with GCN method
+        deg = degree(col, x.size(0), dtype=x.dtype)
+        t_deg = deg[tetra_nei_ids]
+        t_deg_inv_sqrt = t_deg.pow(-0.5)
+        t_norm = 0.5 * t_deg_inv_sqrt.mean(dim=1)
+
+        # switch entries for -1 rdkit labels
+        ccw_mask = parity_atoms[tetra_ids] == -1
+        tetra_nei_ids[ccw_mask] = tetra_nei_ids.clone()[ccw_mask][:, [1, 0, 2, 3]]
+
+        # calculate reps
+        edge_ids = torch.cat([tetra_nei_ids.view(1, -1), tetra_ids.repeat_interleave(4).unsqueeze(0)], dim=0)
+        dense_edge_attr = to_dense_adj(edge_index, batch=None, edge_attr=edge_attr).squeeze(0)
+        edge_reps = dense_edge_attr[edge_ids[0], edge_ids[1], :].view(tetra_nei_ids.size(0), 4, -1)
+        reps = x[tetra_nei_ids] + edge_reps
+
+        return t_norm.unsqueeze(-1) * self.tetra_update(reps)
 
 
 class GINEConv(MessagePassing):
